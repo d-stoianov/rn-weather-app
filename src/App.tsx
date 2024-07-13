dcc4cd5aa1825a3ec2f105cb57617e694678ae3d
Dmytro <dmytrostoianov05@gmail.com>
2024-07-13 02:18:35 +0200

    Initial commit. Setup project with typescript and prettier.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+const App = () => {
+    return (
+        <View>
+            <StatusBar style="light" />
+            <Text>Test</Text>
+        </View>
+    )
+}
+
+export default App
